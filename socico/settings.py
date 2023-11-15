a5d9feab82d890e7e9f29527181cac10f2d78d33
kismX <kism@gmx.net>
2023-11-15 15:11:29 +0100

    database psql

--- socico/settings.py
+++ socico/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'socico.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'socico_db',
+        'USER': 'kismx',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '',    # 5432
     }
 }
+
 
 
 # Password validation
