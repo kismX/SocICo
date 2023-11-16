c9fb0f3d5a36115bac2659a8a7d5f3fb58402526
kismX <kism@gmx.net>
2023-11-16 13:28:12 +0100

    Auto stash before merge of "main" and "origin/main"

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,4 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=60)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> b2368c7 (wiederherstellung)
