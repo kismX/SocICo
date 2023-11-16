a5cf2f4fff85b46c2f24f8043f349ce01bbac148
kismX <kism@gmx.net>
2023-11-16 13:44:46 +0100

    bla

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,19 +7,9 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-<<<<<<< HEAD
     list_display = ['email', 'username', 'is_staff']
     add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('name',)}), )
     fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('name', )}), )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
-=======
-
-    list_display = ['email', 'username', 'is_staff']
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('name', )}), )
-    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('name', )}), )
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
->>>>>>> b2368c7 (wiederherstellung)
