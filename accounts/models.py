c67ef4179c4e4191b5227a229667b9fe858b1d0a
Sarah <sarahpardeike@gmail.com>
2023-11-25 11:32:09 +0100

    fixed duplicated friendship model

--- accounts/models.py
+++ accounts/models.py
@@ -59,17 +59,3 @@ class Friendship(models.Model):
         friends = Friendship.objects.filter(accepted_at__isnull=False)
         return [friend for friend in friends] # spucke mir jeden friend in friends aus (wollt mal wieder dings hier üben.. [ ])
     
-
-
-
-# neues model das eine Friednship darstellt
-# es hat den 'from_user', von dem die anfrage ausgeht und 'to_user' an den die anfrage geht
-# wann  die verbindung erstellt wurde geht automatisch: 'createtd_at' und wenn request angenommen, wird das leere 'acceptet_at' mit zeitpunkt ausgefüllt
-class Friendship(models.Model):
-    from_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='from_user')
-    to_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='to_user')
-    created_at = models.DateTimeField(auto_now_add=True)
-    accepted_at = models.DateTimeField(null=True, blank=True)
-
-    
-
