357e612d34bad1ca03ac8121c8c6d1399010154d
kismX <kism@gmx.net>
2023-12-08 22:29:52 +0100

    + kleine ausbesserungen

--- accounts/signals.py
+++ accounts/signals.py
@@ -22,3 +22,4 @@ def user_logged_in_handler(sender, request, user, **kwargs):
 @receiver(user_logged_out)
 def user_logged_out_handler(sender, request, user, **kwargs):
     update_activity_status(user.profile)
+
