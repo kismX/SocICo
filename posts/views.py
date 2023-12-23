95f3b73103d86c396fd186774a67d1bcb745261c
kismx <kism@gmx.net>
2023-12-23 21:27:34 +0100

    Postings können editiert und deleted werden

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm, EventForm
 from accounts.models import Profile
 
+# views zum posten
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
@@ -10,12 +11,33 @@ def create_post(request):
             post = form.save(commit=False)
             post.user = request.user
             post.save()
-            return redirect('home')
+            return redirect('profile_detail', pk=post.user.id)
     else:
         form = PostForm()
     return render(request, 'posts/create_post.html', {'form': form})
 
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_detail', pk=post.user.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/edit_post.html', {'form': form})
 
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('profile_detail', pk=post.user.id)
+    return render(request, 'posts/delete_post.html', {'post': post})
+
+
+
+
+# views zu events
 def create_event(request):
     if request.method == 'POST':
         form = EventForm(request.POST, request.FILES)
