e203e97c43acd0995acb1f07f9ff9e6f79001f16
Sarah <sarahpardeike@gmail.com>
2023-12-30 10:29:22 +0100

    updated - css, chat
    css der profilliste aktualisiert, im chatroom wird nun der name der anderen person angezeigt

--- chats/views.py
+++ chats/views.py
@@ -36,8 +36,26 @@ def rooms(request):
 def room(request, slug):
     room = Room.objects.get(slug=slug)
     messages = Message.objects.filter(room=room)
+    profiles = get_user_model().objects.all()
+    user_dict = dict()
+    username_list = []
 
-    return render(request, 'chats/room.html', {'room': room, 'messages': messages})
+    split_slug = room.slug.split('_')
+    split_slug = [int(i) for i in split_slug]
+
+    for i in split_slug:
+        if i != request.user.id:
+            for profile in profiles:
+                if i == profile.id:
+                    name = profile.username
+            username_list.append(name)
+
+    if request.user.id in split_slug:
+        user_dict[room.slug] = username_list
+
+    username_list = []
+
+    return render(request, 'chats/room.html', {'room': room, 'messages': messages, 'user_dict': user_dict})
 
 @login_required
 def create_chat(request, own_id, foreign_id):
