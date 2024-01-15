3a65f03b7917cc972baa67247158c7719fbc44f0
kismx <kism@gmx.net>
2024-01-15 10:45:55 +0100

    fixed -  custom context-processors
    anpassungen, jetzt können ewig viele variablen verfügbar gemacht werden

--- basics/context_processors.py
+++ basics/context_processors.py
@@ -2,17 +2,17 @@ from django.contrib.auth.models import AnonymousUser
 from notifications.models import Notification
 
 def make_context_global(request):
+    context = {}
+    
     # wenn wir nicht ausschließen, dass ein request.user nicht unangemeldet sein kann (also Anonymous), geht alles gewaltig schief :D
     if not isinstance(request.user, AnonymousUser):
         notifications_new = Notification.objects.filter(to_user=request.user, is_read=False)
         notifications_new_count = notifications_new.count()
+
+        context = {
+            'notifications_new_count': notifications_new_count,
+        }
+
+        return context
     else:
-        notifications_new_count = None
-    
-    context = {
-        'notifications_new_count': notifications_new_count,
-    }
-
-    return context
-
-# mnoch überlegen, wie man das else bestückt, wenn man andere context_variablen integriert
+        return context
