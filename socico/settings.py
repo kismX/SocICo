615b893b18b587a2b0e943fba9402b5efddb4099
kismX <kism@gmx.net>
2024-01-19 18:32:03 +0100

    update - registration und verlinkung auf zwangsausfüllen bei profile_edit

--- socico/settings.py
+++ socico/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'basics.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'socico.urls'
