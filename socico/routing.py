14a079194439f2558a88a5e92ce1ae0313c4976a
Sarah <sarahpardeike@gmail.com>
2024-01-30 14:49:10 +0100

    fix - consumer

--- socico/routing.py
+++ socico/routing.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from notifications import consumers as notifications_conumer
+from notifications import consumers as notifications_consumer
 from chats import consumers as chats_consumer
 
 websocket_urlpatterns = [
-    path('ws/notifications/', notifications_conumer.NotificationConsumer.as_asgi()),
+    path('ws/notifications/', notifications_consumer.NotificationConsumer.as_asgi()),
     path('ws/<str:room_name>/', chats_consumer.ChatConsumer.as_asgi()),
 ]
